8d2d339ff7a82789ff9c9cd1b15c9ba589af01ae
Amey Patil <97515279+Amey11github@users.noreply.github.com>
2025-09-07 20:23:27 +0530

    Add files via upload

---
+++ LinkedHashSetEx2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class LinkedHashSetEx2
+{
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> set=new LinkedHashSet<>();
+		
+		set.add(100);
+		set.add(200);
+		set.add(300);
+		set.add(400);
+		set.add(500);
+		set.add(600);
+
+		System.out.println(set);
+
+	}
+}
